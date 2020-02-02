873b38b7dfe1956497f6de1d280f7ee79bb313e6
RudrenduMahindar <mahindar@usc.edu>
2020-02-01 16:32:15 -0800

    more tests problem 3

--- tests/solution_test.cc
+++ tests/solution_test.cc
@@ -3,7 +3,7 @@
 #include <vector>
 using namespace std;
 
-TEST(HelloWorldShould, ReturnHelloWorld) {
+TEST(MedianTest, HandlesEvenSizeInput) {
   Solution solution;
   double arr[] = {1,26,13,8,10,20};
   int n = sizeof(arr)/sizeof(arr[0]);
@@ -13,7 +13,27 @@ TEST(HelloWorldShould, ReturnHelloWorld) {
   EXPECT_DOUBLE_EQ(expected, actual);
 }
 
-TEST(HelloWorldShould1, ReturnHelloWorld1) {
+TEST(MedianTest, HandlesOddSizeInput) {
+  Solution solution;
+  double arr[] = {1,24,16,10,20};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  vector<double> v(arr,arr+n);
+  double actual = solution.FindMedian(v);
+  double expected = 16;
+  EXPECT_DOUBLE_EQ(expected, actual);
+}
+
+TEST(MedianTest, HandlesEqualInputs) {
+  Solution solution;
+  double arr[] = {4,4,4,4};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  vector<double> v(arr,arr+n);
+  double actual = solution.FindMedian(v);
+  double expected = 4;
+  EXPECT_DOUBLE_EQ(expected, actual);
+}
+
+TEST(MedianTest, HandlesEmptyInput) {
   Solution solution;
   double arr[] = {};
   int n = sizeof(arr)/sizeof(arr[0]);
